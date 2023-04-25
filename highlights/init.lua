f3d67e6ae0629a8c1c9e68d48dadf5350a807b89
sininen-blue <alaanneil@gmail.com>
2023-04-25 19:30:37 +0800

    added transparency

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,16 @@
 return { -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
+
+	Normal = { bg = "NONE", ctermbg = "NONE" },
+	NormalNC = { bg = "NONE", ctermbg = "NONE" },
+	CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+--	CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+--	CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+	LineNr = {},
+	SignColumn = {},
+	StatusLine = {},
+	NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+	NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+	ZenBg = { bg = "NONE", ctermbg = "NONE" },
+	NotifyBackground = {bg = "#000000"},
 }
